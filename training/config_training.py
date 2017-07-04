1e0abd827129dec7a6ac54e91caed150f9ccbb3e
miguelgfierro <miguelgfierro@users.noreply.github.com>
2017-07-04 16:05:14 +0000

    set config values

--- training/config_training.py
+++ training/config_training.py
@@ -1,9 +1,9 @@
-config = {'stage1_data_path':'/work/DataBowl3/stage1/stage1/',
-          'luna_raw':'/work/DataBowl3/luna/raw/',
-          'luna_segment':'/work/DataBowl3/luna/seg-lungs-LUNA16/',
+config = {'stage1_data_path':'/datadrive/lung_cancer/lung_cancer_dsb/stage1/',
+          'luna_raw':'/datadrive/lung_cancer/luna/',
+          'luna_segment':'/datadrive/lung_cancer/luna/seg-lungs-LUNA16/',
           
-          'luna_data':'/work/DataBowl3/luna/allset',
-          'preprocess_result_path':'/work/DataBowl3/stage1/preprocess/',       
+          'luna_data':'/datadrive/lung_cancer/sol1/allset/',
+          'preprocess_result_path':'/datadrive/lung_cancer/sol1/stage1/preprocess/',       
           
           'luna_abbr':'./detector/labels/shorter.csv',
           'luna_label':'./detector/labels/lunaqualified.csv',
